49d682d98684f3bf4ef7b15ba71050988777e9ab
David Lee <leetdavidu@gmail.com>
2026-05-06 23:21:59 +0800

    initial commit

---
+++ infra/domains.tf
@@ -0,0 +1,32 @@
+locals {
+  vercel_cnames_map = {
+    www = {
+      name    = "www.eslee.io"
+      content = "31001004ba3b35ab.vercel-dns-017.com."
+    }
+  }
+}
+
+# ? Dynamic CNAME records that point to the new vercel DNS records
+# ? https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/dns_record
+resource "cloudflare_dns_record" "eslee_io_vercel_cnames_map" {
+  for_each = local.vercel_cnames_map
+
+  zone_id = var.cloudflare_zone_id
+  type    = "CNAME"
+  name    = each.value.name
+  content = each.value.content
+  ttl     = 1
+  proxied = false
+}
+
+
+# ? Homepage (Manual Config)
+resource "cloudflare_dns_record" "eslee_io_root" {
+  zone_id = var.cloudflare_zone_id
+  type    = "A"
+  name    = "@"
+  content = "216.198.79.1"
+  ttl     = 1
+  proxied = false
+}
---
+++ infra/variables.tf
@@ -0,0 +1,8 @@
+variable "cloudflare_api_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "cloudflare_zone_id" {
+  type = string
+}
